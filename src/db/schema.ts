1425ff6d460d359125cc549cfd173b798cc13b88
sanjaytheeng <sagartamang545@gmail.comg>
2025-07-04 10:06:59 +0545

    feat: add database integration with drizzle-orm and neon
    - Updated package.json to include drizzle-orm and related dependencies.
    - Added database connection setup in src/db/index.ts.
    - Created schema definition for users in src/db/schema.ts.

---
+++ src/db/schema.ts
@@ -0,0 +1,9 @@
+import { integer, pgTable, serial, text } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  email: text("email").notNull().unique(),
+  password: text("password").notNull(),
+  createdAt: integer("created_at").notNull().unique(),
+});
