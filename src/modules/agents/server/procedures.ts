d7952e2a2cd4121ca409def5965af3126d798c28
sanjaytheeng <sagartamang545@gmail.comg>
2025-07-20 10:30:25 +0545

    feat: implement agent management features; add agent creation form and dialog; integrate protected procedures for agent operations; enhance agents list with header component

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,11 +1,42 @@
 import { db } from "@/db";
 import { agents } from "@/db/schema";
-import { createTRPCRouter, baseProcedure } from "@/trpc/init";
+import {
+  createTRPCRouter,
+  baseProcedure,
+  protectedProcedure,
+} from "@/trpc/init";
+import { agentsInsertSchema } from "../schemas";
+import { z } from "zod";
+import { eq } from "drizzle-orm";
 
 export const agentsRouter = createTRPCRouter({
+  // TODO: Change `getOne` to use `protectedProcedure`
+  getOne: baseProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input }) => {
+      const [existingAgent] = await db
+        .select()
+        .from(agents)
+        .where(eq(agents.id, input.id));
+
+      return existingAgent;
+    }),
+  // TODO: Change `getMany` to use `protectedProcedure`
   getMany: baseProcedure.query(async () => {
     const data = await db.select().from(agents);
 
     return data;
   }),
+  create: protectedProcedure
+    .input(agentsInsertSchema)
+    .mutation(async ({ input, ctx }) => {
+      const [createAgent] = await db
+        .insert(agents)
+        .values({
+          ...input,
+          userID: ctx.auth.user.id,
+        })
+        .returning();
+      return createAgent;
+    }),
 });
