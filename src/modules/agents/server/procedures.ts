26a06f386f867ea8349e8e8cedea835fc77f0cda
sanjaytheeng <sagartamang545@gmail.comg>
2025-07-21 18:52:04 +0545

    feat: implement agent detail view with error and loading states; enhance agents view with pagination and row click navigation

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -10,12 +10,13 @@ import {
   MAX_PAGE_SIZE,
   MIN_PAGE_SIZE,
 } from "@/constants";
+import { TRPCError } from "@trpc/server";
 
 export const agentsRouter = createTRPCRouter({
   // TODO: Change `getOne` to use `protectedProcedure`
   getOne: protectedProcedure
     .input(z.object({ id: z.string() }))
-    .query(async ({ input }) => {
+    .query(async ({ input, ctx }) => {
       const [existingAgent] = await db
         .select({
           //TODO: change to actual count
@@ -23,7 +24,13 @@ export const agentsRouter = createTRPCRouter({
           meetingCount: sql<number>`5`,
         })
         .from(agents)
-        .where(eq(agents.id, input.id));
+        .where(
+          and(eq(agents.id, input.id), eq(agents.userID, ctx.auth.user.id))
+        );
+
+      if (!existingAgent) {
+        throw new TRPCError({ code: "NOT_FOUND", message: "Agent not found" });
+      }
 
       return existingAgent;
     }),
